ae93f40b22b804aa7e8998bde2e9d16249ea1e7b
Sid He <xhe518@gmail.com>
2018-05-17 20:54:58 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,18 @@
 class Dog
-  def initialize(name, breed)
-    @name = name
-    @breed = breed
+  def name=(dogs_name)
+    @name = dogs_name
   end
 
   def name
     @name
   end
 
-  def name=(new_name)
-    @name = new_name
+  def breed=(new_breed)
+    @breed = new_breed
   end
 
   def breed
     @breed
-  end
-
-  def breed=(new_breed)
-    @breed = new_breed
   end
 
 end
