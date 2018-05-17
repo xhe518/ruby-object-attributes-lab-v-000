c728243f9440b4dd5a988bbed81b25df1f550680
Sid He <xhe518@gmail.com>
2018-05-17 20:23:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,16 +11,4 @@ class Dog
     @name
   end
 
-  def initialize(breed)
-    @breed = breed
-  end
-
-  def breed=(new_breed)
-    @breed = new_breed
-  end
-
-  def breed
-    @breed
-  end
-
 end
